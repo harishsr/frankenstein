f61329dfd490ebdb2fc80b5cb066f29fef936ac0
Harish Ramachandran <harishsr.coder@gmail.com>
2015-10-13 22:03:27 -0400

    Tests for CREATE
    Wrote the tests & made them pass.  Allowed for validating the title &
    content to exist & have a length range.  Gave the view an error
    notification.  Bootstrap integration is still required.

--- app/models/blog.rb
+++ app/models/blog.rb
@@ -1,2 +1,4 @@
 class Blog < ActiveRecord::Base
+  validates :title,   presence: true, length: { in: 3..100 }
+  validates :content, presence: true, length: { in: 10..10_000 }
 end
--- spec/features/blog_spec.rb
+++ spec/features/blog_spec.rb
@@ -2,7 +2,6 @@ require 'rails_helper'
 
 RSpec.feature "Blogs", type: :feature do
   context "Creating an entry" do 
-    
     before do 
       visit '/'
       @title = 'Yo, Dracula!'
@@ -10,19 +9,40 @@ RSpec.feature "Blogs", type: :feature do
     end
 
     it "should be successful" do 
-      click_link "New entry"
-      fill_in 'Title', with: @title
-      fill_in 'Content', with: @post
-      click_button 'Create Blog'
-
+      create_entry(@title, @post)
       expect(page).to have_content(@title)
       expect(page).to have_content(@post)
     end
 
     it "should properly fail if validations aren't being met" do 
-    end
+      create_entry("", @post)
+      expect(page).to have_content("can't be blank")
 
-    it "should allow the entry of an image" do 
+      create_entry(@title, "")
+      expect(page).to have_content("can't be blank")
+
+      create_entry("hi", @post)
+      expect(page).to have_content("is too short")
+
+      create_entry(@title, "hello")
+      expect(page).to have_content("is too short")
+
+      create_entry("I"*101, @post)
+      expect(page).to have_content("is too long")
+
+      create_entry(@title, "I"*10_001)
+      expect(page).to have_content("is too long")
+
     end
   end
+
+  private
+
+    def create_entry(title = @title, post = @post)
+      visit '/'
+      click_link "New entry"
+      fill_in 'Title', with: title
+      fill_in 'Content', with: post
+      click_button 'Create Blog'
+    end
 end
