9bc26b5dba9f86173d4eb5384c828453bfc7eaa7
Harish Ramachandran <harishsr@beardendesigns.com>
2016-01-13 14:38:17 -0500

    added nested attributes

--- app/models/request.rb
+++ app/models/request.rb
@@ -1,4 +1,6 @@
 class Request < ActiveRecord::Base
   belongs_to :user
   has_many :years, through: :user
+
+  accepts_nested_attributes_for :user
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,6 @@
 class User < ActiveRecord::Base
   has_many :requests
   has_many :years
+  
+  accepts_nested_attributes_for :years
 end
